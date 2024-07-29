9a70f3ab159b1b9ac7befc4a4185ba04cb997df4
Aaron Longwell <a@adl.io>
2024-07-28 22:54:47 -0700

    WIP

--- src/background.rs
+++ src/background.rs
@@ -1,8 +1,7 @@
 use std::fs;
-use std::io;
-use std::os::unix::process::CommandExt;
-use std::process::{Child, Command};
-use std::time::Duration;
+use std::process::Command;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use sysinfo::System;
 
 use crate::Result;
 use crate::Workflow;
@@ -13,6 +12,7 @@ use crate::Workflow;
 ///
 pub enum BackgroundTaskStatus {
     Fresh(Duration),
+    // TODO: This should wrap a struct to clarify the meaning of the durations
     StaleAndRunning(Duration, Duration),
     StaleAndFailed(Duration, String),
 }
@@ -23,47 +23,97 @@ pub struct BGTask {
     pub ttl: Duration,
 }
 
+pub struct BGTaskExecutionResult {
+    pub last_completion: chrono::DateTime<chrono::Utc>,
+}
+
 impl Workflow {
-    pub fn run_in_background(&self, job_name: &str, mut cmd: Command) -> io::Result<Child> {
-        if self.is_running(job_name) {
-            let pid = self.get_pid(job_name)?;
-            return Err(io::Error::new(
-                io::ErrorKind::AlreadyExists,
-                ErrJobExists {
-                    job_name: job_name.to_string(),
-                    pid,
-                },
-            ));
+    pub fn run_in_background(
+        &self,
+        job_name: &str,
+        max_age: Duration,
+        mut cmd: Command,
+    ) -> Result<BackgroundTaskStatus> {
+        // TODO This should be a let staleness = ...
+        match self.get_duration_since_last_completion(job_name) {
+            Some(duration) => {
+                if duration < max_age {
+                    return Ok(BackgroundTaskStatus::Fresh(duration));
+                }
+            }
+            None => {}
         }
 
-        // Set the process group ID to prevent the process from being killed when the parent is
-        unsafe {
-            cmd.pre_exec(|| {
-                libc::setpgid(0, 0);
-                Ok(())
-            });
+        match self.get_pid_running_duration(job_name) {
+            Some(duration) => {
+                return Ok(BackgroundTaskStatus::StaleAndRunning(duration, duration));
+            }
+            None => {
+                // TODO Fix this
+                Ok(BackgroundTaskStatus::Fresh(self.max_age))
+            }
         }
-
-        let child = cmd.spawn()?;
-
-        self.save_pid(job_name, child.id())?;
-
-        Ok(child)
     }
 
     fn is_running(&self, job_name: &str) -> bool {
-        self.get_pid(job_name).is_ok()
+        match self.get_pid(job_name) {
+            Ok(pid) => {
+                let mut system = System::new_all();
+                system.refresh_processes();
+                system.process(sysinfo::Pid::from(pid as usize)).is_some()
+            }
+            Err(_) => false,
+        }
+    }
+
+    fn get_pid_running_duration(&self, job_name: &str) -> Option<Duration> {
+        match self.get_pid(job_name) {
+            Ok(pid) => {
+                let mut system = System::new_all();
+                system.refresh_processes();
+                system.process(sysinfo::Pid::from(pid as usize)).map(|p| {
+                    let start_time = UNIX_EPOCH + Duration::from_secs(p.start_time());
+                    SystemTime::now()
+                        .duration_since(start_time)
+                        .unwrap_or_default()
+                })
+            }
+            Err(_) => None,
+        }
     }
 
     fn get_pid(&self, job_name: &str) -> Result<u32> {
         let pid_file = self.cache_dir.join(format!("{}.pid", job_name));
-        let pid_str = fs::read_to_string(pid_file)?;
-        pid_str.trim().parse::<u32>().map_err(|e| e.into())
+        let pid = fs::read_to_string(pid_file)?;
+        pid.trim().parse::<u32>().map_err(|e| e.into())
     }
 
     fn save_pid(&self, job_name: &str, pid: u32) -> Result<()> {
         let pid_file = self.cache_dir.join(format!("{}.pid", job_name));
         fs::write(pid_file, pid.to_string())?;
+        Ok(())
+    }
+
+    /// If the specified job has successfully completed before, this returns the duration
+    /// since that event occurred. Otherwise, it returns None. We use the file timestamp
+    /// on an empty file to determine the last completion time.
+    fn get_duration_since_last_completion(&self, job_name: &str) -> Option<Duration> {
+        let last_completion_file = self.cache_dir.join(format!("{}.last", job_name));
+        match fs::metadata(last_completion_file) {
+            Ok(metadata) => {
+                let last_completion = metadata.modified().unwrap();
+                let duration = SystemTime::now().duration_since(last_completion).unwrap();
+                Some(duration)
+            }
+            Err(_) => None,
+        }
+    }
+
+    fn update_last_completion(&self, job_name: &str) -> Result<()> {
+        let last_completion_file = self.cache_dir.join(format!("{}.last", job_name));
+        // We write the time in the local time zone. This is mainly for human legibility.
+        // We actually use the modified timestamp of the file to determine the age
+        fs::write(last_completion_file, chrono::Local::now().to_rfc3339())?;
         Ok(())
     }
 }
--- src/workflow.rs
+++ src/workflow.rs
@@ -1,11 +1,28 @@
 use std::path::PathBuf;
 
-use crate::Result;
+use crate::{Item, Result};
+
+const VAR_WORKFLOW_NAME: &str = "alfred_workflow_name";
+const VAR_WORKFLOW_BUNDLE_ID: &str = "alfred_workflow_bundleid";
+const VAR_WORKFLOW_VERSION: &str = "alfred_workflow_version";
+const VAR_WORKFLOW_UID: &str = "alfred_workflow_uid";
+const VAR_WORKFLOW_CACHE_DIR: &str = "alfred_workflow_cache";
+const VAR_WORKFLOW_DATA_DIR: &str = "alfred_workflow_data";
+const VAR_DEBUG: &str = "alfred_debug";
+const VAR_THEME: &str = "alfred_theme";
+const VAR_THEME_BG: &str = "alfred_theme_background";
+const VAR_THEME_SELECTION_BG: &str = "alfred_theme_selection_background";
+const VAR_VERSION: &str = "alfred_version";
+const VAR_VERSION_BUILD: &str = "alfred_version_build";
+const VAR_PREFERENCES: &str = "alfred_preferences";
+const VAR_PREFERENCES_LOCAL_HASH: &str = "alfred_preferences_localhash";
 
 pub struct Workflow {
     pub name: String,
     pub data_dir: PathBuf,
     pub cache_dir: PathBuf,
+
+    pub items: Vec<Item>,
 }
 
 impl Workflow {
@@ -16,14 +33,16 @@ impl Workflow {
     /// See https://www.alfredapp.com/help/workflows/script-environment-variables/
     /// for more information on the Alfred workflow environment variables.
     pub fn new() -> Result<Workflow> {
-        let name = std::env::var("alfred_workflow_name")?;
-        let cache_dir = std::env::var("alfred_workflow_cache")?;
-        let data_dir = std::env::var("alfred_workflow_data")?;
+        let name = std::env::var(VAR_WORKFLOW_NAME)?;
+        let cache_dir = std::env::var(VAR_WORKFLOW_CACHE_DIR)?;
+        let data_dir = std::env::var(VAR_WORKFLOW_DATA_DIR)?;
+
         Ok({
             Workflow {
                 name,
                 data_dir: PathBuf::from(data_dir),
                 cache_dir: PathBuf::from(cache_dir),
+                items: Vec::new(),
             }
         })
     }
