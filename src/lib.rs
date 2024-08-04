ed23298dfae163ba27638ae46f520773fdad38e1
Aaron Longwell <a@adl.io>
2024-08-03 20:59:41 -0700

    Add use for WorkflowConfig. Refactor imports.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
+pub use cache::cache_dir;
+pub use clipboard::copy_markdown_link_to_clipboard;
+pub use clipboard::copy_rich_text_link_to_clipboard;
+pub use data::data_dir;
+pub use error::{Error, Result};
+pub use item::{filter_and_sort_items, Icon, Item, Key, Modifier};
+pub use response::Response;
+pub use url_item::URLItem;
+pub use workflow::Workflow;
+pub use workflow_config::WorkflowConfig;
+
 mod background;
 mod cache;
 mod clipboard;
@@ -8,19 +19,7 @@ mod item;
 mod response;
 mod url_item;
 mod workflow;
-
-pub use item::{filter_and_sort_items, Icon, Item, Key, Modifier};
-pub use response::Response;
-pub use url_item::URLItem;
-
-pub use cache::cache_dir;
-pub use data::data_dir;
-pub use error::{Error, Result};
-
-pub use clipboard::copy_markdown_link_to_clipboard;
-pub use clipboard::copy_rich_text_link_to_clipboard;
-
-pub use workflow::Workflow;
+mod workflow_config;
 
 pub fn handle() {
     clipboard::handle_clipboard()
