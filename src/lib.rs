929e7d9f93cab27400f12734bc5bd8faa8b5c303
Aaron Longwell <a@adl.io>
2024-09-15 21:44:23 -0700

    Remove unused data and cache mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,23 @@
-pub use cache::cache_dir;
-pub use clipboard::copy_markdown_link_to_clipboard;
-pub use clipboard::copy_rich_text_link_to_clipboard;
-pub use data::data_dir;
-pub use error::{Error, Result, WorkflowError};
-pub use icon::{Icon, *};
-pub use item::{filter_and_sort_items, Item, Key, Modifier};
-pub use response::Response;
-pub use url_item::URLItem;
-pub use workflow::Workflow;
-
 mod background;
 mod background_job;
-mod cache;
 mod clipboard;
-pub mod config;
-mod data;
+mod config;
 mod error;
 mod icon;
 mod item;
 mod response;
 mod url_item;
 mod workflow;
+
+pub use clipboard::copy_markdown_link_to_clipboard;
+pub use clipboard::copy_rich_text_link_to_clipboard;
+pub use error::{Error, Result, WorkflowError, WorkflowResult};
+pub use icon::{Icon, *};
+pub use item::{filter_and_sort_items, Item, Key, Modifier};
+pub use response::Response;
+pub use url_item::URLItem;
+pub use workflow::{AsyncRunnable, Runnable, Workflow};
+pub use workflow_config::WorkflowConfig;
 
 pub fn handle() {
     clipboard::handle_clipboard()
