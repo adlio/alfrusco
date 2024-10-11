0659e10e108066afe0ab82734db257844eb8da6e
Aaron Longwell <a@adl.io>
2024-10-11 10:53:57 -0700

    Fix compatibility with newer sysinfo

--- src/background_job.rs
+++ src/background_job.rs
@@ -218,7 +218,7 @@ impl<'a> BackgroundJob<'a> {
     ///
     fn get_running_duration(&self) -> Option<Duration> {
         let mut system = System::new_all();
-        system.refresh_processes(sysinfo::ProcessesToUpdate::All);
+        system.refresh_processes(sysinfo::ProcessesToUpdate::All, true);
 
         let pid = self.get_pid();
         match pid {
